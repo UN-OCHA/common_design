0885990756abd274aa0ed8e26a072824a554d3b5
left23 <lcdemc@gmail.com>
2018-10-18 10:55:06 +0200

    aria attributes for main menu, as per suggested bootstrap

--- js/custom-menu.js
+++ js/custom-menu.js
@@ -7,7 +7,17 @@
   Drupal.behaviors.cdMenu = {
     attach: function (context, settings) {
       $('.menu--main .menu-item--expanded > a', context).on('click', function (e) {
+
         $(this).parent().toggleClass('open');
+
+        var menuItem = $( e.currentTarget );
+
+        if (menuItem.attr( 'aria-expanded') === 'true') {
+          $(this).attr( 'aria-expanded', 'false');
+        } else {
+          $(this).attr( 'aria-expanded', 'true');
+        }
+
         e.stopPropagation();
         e.preventDefault();
       });
