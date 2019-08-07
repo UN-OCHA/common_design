f0e63e4a80422cc063f2a7a670cde24443d3dede
left23 <lcdemc@gmail.com>
2019-08-07 17:40:46 +0200

    use class selectors instead of ids, as per change to ocha_basic, start with vanilla version, commented out jquery

--- js/custom-search.js
+++ js/custom-search.js
@@ -3,23 +3,31 @@
  * Bootstrap dropdown on search.
  */
 
-(function ($, Drupal) {
-  Drupal.behaviors.cdSearch = {
-    attach: function (context, settings) {
+// (function ($, Drupal) {
+//   Drupal.behaviors.cdSearch = {
+//     attach: function (context, settings) {
+//
+//       // Apply focus to input when dropdown is shown.
+//       $('.cd-search').on('shown.bs.dropdown', function () {
+//         $(this).find('.cd-search__input').focus();
+//       }).on('hidden.bs.dropdown', function () {
+//         $(this).find('.cd-search__input').blur();
+//       });
+//
+//       $('.cd-search__input').on('focus', function (e) {
+//         $(this).parents('.cd-search__form').find('.cd-search__submit').addClass('js-has-focus');
+//       }).on('blur', function (e) {
+//         $(this).parents('.cd-search__form').find('.cd-search__submit').removeClass('js-has-focus');
+//       });
+//
+//     }
+//   };
+// }(jQuery, Drupal));
 
-      // Apply focus to input when dropdown is shown.
-      $('.cd-search').on('shown.bs.dropdown', function () {
-        $(this).find('#cd-search').focus();
-      }).on('hidden.bs.dropdown', function () {
-        $(this).find('#cd-search').blur();
-      });
+Drupal.behaviors.cdSearch = {
+  attach: function (context, settings) {
 
-      $('.cd-search__input').on('focus', function (e) {
-        $(this).parents('.cd-search__form').find('.cd-search__submit').addClass('js-has-focus');
-      }).on('blur', function (e) {
-        $(this).parents('.cd-search__form').find('.cd-search__submit').removeClass('js-has-focus');
-      });
+      console.log('cdSearch');
 
-    }
-  };
-}(jQuery, Drupal));
+  }
+};
