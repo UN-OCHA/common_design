8b30bd6bdcc882f8805b5eabcf29fd2d9a0cecd3
left23 <lcdemc@gmail.com>
2020-04-08 16:40:25 +0200

    add filter component, with styles, js for display toggle on mobile, twig template with library attached

---
+++ components/cd-filters/cd-filters.js
@@ -0,0 +1,20 @@
+const filtersButton = document.querySelector('.cd-filters__btn');
+
+// Media query event handler.
+if (matchMedia) {
+  const mq = window.matchMedia('(max-width: 991px)');
+  // Add event listener to media query that fires when a change in viewport width is detected.
+  mq.addListener(WidthChange);
+  // Call handler to initialise on page load.
+  WidthChange(mq);
+}
+
+function WidthChange(mq) {
+  if (mq.matches) {
+    filtersButton.addEventListener('click', toggleFilters);
+  }
+}
+
+function toggleFilters() {
+  this.closest('div').classList.toggle('js-filtersOpen');
+}
