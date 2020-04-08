244863f6be5446068aa8f4f4e990f06ae627ea97
left23 <lcdemc@gmail.com>
2020-04-08 17:03:14 +0200

    js function, needs aria-expanded

--- components/cd-filters/cd-filters.js
+++ components/cd-filters/cd-filters.js
@@ -1,20 +1,25 @@
-const filtersButton = document.querySelector('.cd-filters__btn');
+function cdFiltersToggle() {
+  const filtersButton = document.querySelector('.cd-filters__btn');
 
 // Media query event handler.
-if (matchMedia) {
-  const mq = window.matchMedia('(max-width: 991px)');
-  // Add event listener to media query that fires when a change in viewport width is detected.
-  mq.addListener(WidthChange);
-  // Call handler to initialise on page load.
-  WidthChange(mq);
-}
-
-function WidthChange(mq) {
-  if (mq.matches) {
-    filtersButton.addEventListener('click', toggleFilters);
+  if (matchMedia) {
+    const mq = window.matchMedia('(max-width: 991px)');
+    // Add event listener to media query that fires when a change in viewport width is detected.
+    mq.addListener(WidthChange);
+    // Call handler to initialise on page load.
+    WidthChange(mq);
   }
-}
 
-function toggleFilters() {
-  this.closest('div').classList.toggle('js-filtersOpen');
-}
+  function WidthChange(mq) {
+    if (mq.matches) {
+      filtersButton.addEventListener('click', toggleFilters);
+    }
+  }
+
+  function toggleFilters() {
+    this.closest('div').classList.toggle('js-filtersOpen');
+  }
+
+};
+
+cdFiltersToggle();
