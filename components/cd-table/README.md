6109ddd0bed87ae68a644d00779379092030e6c8
left23 <lcdemc@gmail.com>
2021-02-04 12:40:47 +0100

    cd-table component with wrapping container to add horizontal scrolling when viewport is too narrow, js to add role and tabindex and markup to display tables with a variety of data

--- components/cd-table/README.md
+++ components/cd-table/README.md
@@ -1,16 +1,27 @@
 # Table
 
 ## Purpose and Usage
-HTML table for complex data, using `data-` attribute selectors for styling specific column data. On mobile, the table displays as cards.
+HTML table for displaying simple tabular data. 
 
-@TODO
-Rework
+On narrow viewports, when the table width exceeds the available space, a horizontal scroll on the wrapping div appears, the tabindex attribute is added and set to 0, and the role of "group" is added.
+See https://inclusive-components.design/data-tables
+
+The component styles use `data-` attribute selectors for styling specific column data, as an example of how specific data can be targeted.
+
+@TODO add a message to prompt scrolling.
+@TODO expand this component to include a variant for when the data is too dense to display well on narrow viewports
+even with the horizontal scroll. In that case, we could output the table data as a definition list instead. See CD-219 for more.
 
 ## Caveats
+The table element must have a wrapping div `cd-table-container` for the horizontal scrolling and javascript is needed to
+add the tabindex and role.
+Sometimes this creates a horizontal scroll on the body, depending on the surrounding layout and existing CSS rules. 
+We want to avoid using overflow rules on the body if possible.
 
 ### Variants
 
 ```
-none
+cd-table--row-numbers
+cd-table--striped
 
 ```
--- components/cd-table/cd-table.js
+++ components/cd-table/cd-table.js
@@ -1,22 +1,41 @@
-// Table headings added as data-content to table cells for cards on mobile
-function cdResponsiveTables() {
-  var table = document.querySelector('.cd-table--responsive');
-  var tableRows = table.querySelectorAll('tbody tr');
-  var tableHeadings = table.querySelectorAll('th');
-  var tableHeadingCollection = Array.from(tableHeadings);
+/**
+ * Detect whether the table content overflows the available width.
+ * Add tabindex and role if scrollWidth exceeds clientWidth for the container.
+ * @TODO add a message to prompt scrolling.
+ */
+(function () {
+  'use strict';
 
-  for (var i = 0; i < tableRows.length; i++) {
-    var tableCells = tableRows[i].querySelectorAll('td');
-    var tableCellsCollection = Array.from(tableCells);
+  var elements = document.querySelectorAll('.cd-table-container');
 
-    for (var j = 0; j < tableCellsCollection.length; j++) {
-      if (j === tableHeadingCollection.length) {
-        continue;
+  function updateTable(event) {
+    for (var i = 0, l = elements.length; i < l; i++) {
+      var element = elements[i];
+      var scrollWidth, clientWidth = element;
+      var isOverflowing = element.scrollWidth > element.offsetWidth;
+
+      if (isOverflowing) {
+        element.setAttribute('tabindex', '0');
+        element.setAttribute('role', 'group');
+      } else {
+        element.removeAttribute('tabindex');
+        element.removeAttribute('role');
       }
-      var tableHeadingLabel = tableHeadingCollection[j].innerHTML;
-      tableCellsCollection[j].setAttribute('data-content', tableHeadingLabel);
     }
   }
-};
 
-cdResponsiveTables();
+  if (typeof window.ResizeObserver !== 'undefined') {
+    new ResizeObserver(updateTable).observe(document.documentElement);
+  }
+  // Use an iframe to detect the resizing of the inner width of the window if
+  // ResizeObserver is not supported as we cannot use window.resize for that.
+  else {
+    var iframe = document.createElement('iframe');
+    iframe.style.cssText = 'position:absolute;width:100%;height:0;border:none;visibility:hidden;';
+    iframe.onload = function () {
+      iframe.contentWindow.onresize = updateScrollBarWidth;
+      updateScrollBarWidth();
+    }
+    document.documentElement.appendChild(iframe);
+  }
+})();
