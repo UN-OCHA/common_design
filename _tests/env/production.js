da120eba3154711fe93be8d02fb558f9ef61d79e
left23 <lcdemc@gmail.com>
2021-03-10 15:40:59 +0100

    add gitignore for feature config

--- _tests/env/production.js
+++ _tests/env/production.js
@@ -1,6 +1,6 @@
 const env = {
   timeout: 5000,
-  baseUrl: 'https://commondesign.test'
+  baseUrl: 'https://feature.commondesign-unocha-org.ahconu.org/'
 };
 
 console.log('⚠️  E2E testing', env.baseUrl);
