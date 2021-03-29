117010462451edad6efde8fc8e94b9556d6b954d
left23 <lcdemc@gmail.com>
2021-03-29 17:12:55 +0200

    improve footer menu test so it passes if there are other menu items in footer menu

--- _tests/FooterMenu.test.js
+++ _tests/FooterMenu.test.js
@@ -11,17 +11,15 @@ describe('FooterLinks', () => {
   it('should contain specific links in the Footer menu', async () => {
     const footerMenuItems = [
       'Privacy',
-      'Terms of Use'
+      'Terms of use'
     ];
     const footerMenuItemsHref = [
-      'http://www.un.org/en/sections/about-website/privacy-notice/',
-      'http://www.un.org/en/sections/about-website/terms-use/'
+      'https://www.un.org/en/sections/about-website/privacy-notice',
+      'https://www.un.org/en/sections/about-website/terms-use'
     ];
-    const footerLinks = await page.$$eval('.cd-footer ul a', text => { return text.map(text => text.textContent).slice(0, 2) });
-    const footerLinksHref = await page.$$eval('.cd-footer ul a', anchors => { return anchors.map(anchor => anchor.href).slice(0, 2) });
-    // console.log(footerLinks);
-    // console.log(footerLinksHref);
-    await expect(footerLinks).toEqual(footerMenuItems);
-    await expect(footerLinksHref).toEqual(footerMenuItemsHref);
+    const footerLinks = await page.$$eval('.cd-footer ul a', text => { return text.map(text => text.textContent) });
+    const footerLinksHref = await page.$$eval('.cd-footer ul a', anchors => { return anchors.map(anchor => anchor.href) });
+    await expect(footerLinks).toEqual(expect.arrayContaining(footerMenuItems));
+    await expect(footerLinksHref).toEqual(expect.arrayContaining(footerMenuItemsHref));
   });
 });
