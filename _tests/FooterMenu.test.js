facf96442d4dadaeb99aad4fce8584b006e92ca9
left23 <lcdemc@gmail.com>
2021-04-08 08:53:44 +0200

    updatefooter URLs

--- _tests/FooterMenu.test.js
+++ _tests/FooterMenu.test.js
@@ -14,8 +14,8 @@ describe('Basetheme: FooterLinks', () => {
       'Terms of use'
     ];
     const footerMenuItemsHref = [
-      'https://www.un.org/en/sections/about-website/privacy-notice',
-      'https://www.un.org/en/sections/about-website/terms-use'
+      'https://www.un.org/en/about-us/privacy-notice',
+      'https://www.un.org/en/about-us/terms-of-use'
     ];
     const footerLinks = await page.$$eval('.cd-footer ul a', text => { return text.map(text => text.textContent) });
     const footerLinksHref = await page.$$eval('.cd-footer ul a', anchors => { return anchors.map(anchor => anchor.href) });
