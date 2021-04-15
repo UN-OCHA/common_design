946c1abdde4d939cec2611f53f25907d5dbb5535
left23 <lcdemc@gmail.com>
2021-04-15 16:37:57 +0200

    Correct JS linting errors for tests

--- _tests/MobileMenu.test.js
+++ _tests/MobileMenu.test.js
@@ -3,11 +3,11 @@ import env from './_env';
 jest.setTimeout(env.timeout);
 
 describe('MobileMenu', () => {
-  beforeAll(async () => {
+  beforeAll(async() => {
     await page.goto(`${env.baseUrl}`);
   });
 
-  it('should expand when clicked', async () => {
+  it('should expand when clicked', async() => {
     const toggle = await page.$('.cd-nav__btn');
     await toggle.click();
     const hidden = await page.$eval('.cd-site-header__nav', el => el.dataset.cdHidden);
--- common_design_subtheme/_tests/SearchDropdown.test.js
+++ common_design_subtheme/_tests/SearchDropdown.test.js
@@ -3,11 +3,11 @@ import env from './_env';
 jest.setTimeout(env.timeout);
 
 describe('SearchDropdown', () => {
-  beforeAll(async () => {
+  beforeAll(async() => {
     await page.goto(`${env.baseUrl}`);
   });
 
-  it('should expand when clicked', async () => {
+  it('should expand when clicked', async() => {
     const toggle = await page.$('.cd-search__btn');
     await toggle.click();
     const hidden = await page.$eval('.cd-search__form', el => el.dataset.cdHidden);
