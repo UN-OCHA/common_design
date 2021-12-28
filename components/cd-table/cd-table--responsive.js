4c0733253743ff5b430b01484c4850be8e134bb4
left23 <lcdemc@gmail.com>
2021-12-28 10:43:00 -0700

    js for checking if the table is larger than the available space, add attributes, add message

--- components/cd-table/cd-table--responsive.js
+++ components/cd-table/cd-table--responsive.js
@@ -1,73 +1,83 @@
 /**
  * Detect whether the table content overflows the available width.
  * Add tabindex and role if scrollWidth exceeds clientWidth for the container.
- * @TODO add a message to prompt scrolling.
+ * Add a message to prompt scrolling.
  */
 
-(function () {
+(function (Drupal) {
   'use strict';
 
-  function updateTable(table) {
-    console.log('update table');
+  Drupal.behaviors.cdTables = {
+    attach: function (context, settings) {
+      document.documentElement.classList.remove('no-js');
+      // Bind the event handlers so that `this` corresponds to the current
+      // object and can be used inside the event handling functions.
+      this.handleResize = this.handleResize.bind(this);
 
-    if (table.classList.contains('cd-table-container-processed')) {
-      return;
-    }
-    table.classList.add('cd-table-container-processed');
+      // Initialize tables.
+      this.initializeTables();
+    },
 
-    var elements = document.querySelectorAll('.cd-table-container-processed');
+    // Update the table container elements when the window is resized.
+    handleResize: function () {
+      var elements = document.querySelectorAll('[data-cd-scrollable');
+      for (var i = 0; i < elements.length; i++) {
+        this.updateTable(elements[i]);
+      }
+    },
 
-    for (var i = 0, l = elements.length; i < l; i++) {
-      var element = elements[i];
+    updateTable: function(element) {
+      // Determine if the table width exceeds the available space.
       var isOverflowing = element.scrollWidth > element.clientWidth;
 
-      if (isOverflowing) {
-        element.setAttribute('tabindex', '0');
-        element.setAttribute('role', 'group');
-        console.log(isOverflowing);
-        createMessage(element);
+      if (!isOverflowing) {
+        this.unsetTableScroll(element);
       }
       else {
-        element.removeAttribute('tabindex');
-        element.removeAttribute('role');
-        console.log(isOverflowing);
+        this.setTableScroll(element);
       }
+      // Mark the element as processed.
+      if (!element.hasAttribute('cd-table-container-processed')) {
+        element.setAttribute('cd-table-container-processed', true);
+      }
+    },
+
+    setTableScroll: function (element) {
+      var messageInCaption = element.hasAttribute('tabindex');
+      element.setAttribute('tabindex', '0');
+      element.setAttribute('role', 'group');
+      if (!messageInCaption) {
+        this.createMessage(element);
+      }
+    },
+
+    unsetTableScroll: function (element) {
+      element.removeAttribute('tabindex');
+      element.removeAttribute('role');
+
+      var tableMessage = element.querySelector('.cd-table--message');
+      if (tableMessage !== null) {
+        var messageDiv = element.querySelector('.cd-table--message');
+        messageDiv.parentNode.removeChild(messageDiv);
+      }
+    },
+
+    // Create the message to prompt scrolling.
+    createMessage: function (element) {
+      var tableCaption = element.querySelector('caption');
+      var messageText = element.getAttribute('data-cd-scrollable') || 'Scroll to see more';
+      var messageDiv = document.createElement('div');
+      messageDiv.classList.add('cd-table--message');
+      messageDiv.appendChild(document.createTextNode(messageText));
+      tableCaption.appendChild(messageDiv);
+    },
+
+    // Initialize the tables, and event handling.
+    initializeTables: function() {
+      window.addEventListener('resize', this.handleResize);
+
+      // Initial setup.
+      this.handleResize();
     }
-  }
-
-  function createMessage(element) {
-    console.log('no caption');
-    const newDiv = document.createElement('div');
-    newDiv.classList.add('cd-table--message');
-    const newContent = document.createTextNode('Scroll to see more');
-    newDiv.appendChild(newContent);
-    const tableCaption = document.getElementById('cd-table__caption');
-    tableCaption.appendChild(newDiv);
-  }
-
-  // Find all non processed `cd-table-container` divs and update them.
-  function updateTables() {
-    var tables = document.querySelectorAll('.cd-table-container:not(.cd-table-container-processed)');
-    console.log('update tables');
-    for (var i = 0, l = tables.length; i < l; i++) {
-      updateTable(tables[i]);
-    }
-  }
-
-  updateTables();
-
-  if (typeof window.ResizeObserver !== 'undefined') {
-    new ResizeObserver(updateTables).observe(document.documentElement);
-  }
-  // Use an iframe to detect the resizing of the inner width of the window if
-  // ResizeObserver is not supported as we cannot use window.resize for that.
-  else {
-    var iframe = document.createElement('iframe');
-    iframe.style.cssText = 'position:absolute;width:100%;height:0;border:none;visibility:hidden;';
-    iframe.onload = function () {
-      iframe.contentWindow.onresize = updateScrollBarWidth;
-      updateScrollBarWidth();
-    }
-    document.documentElement.appendChild(iframe);
-  }
-})();
+  };
+})(Drupal);
