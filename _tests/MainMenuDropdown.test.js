f85cd50a618cbdf5e632ba2ee850c48320504383
Chris Ruppel <ruppel@reliefweb.int>
2023-01-31 10:38:45 +0100

    test: fix E2E MainMenuDropdown
    Refs: CD-392

--- _tests/MainMenuDropdown.test.js
+++ _tests/MainMenuDropdown.test.js
@@ -3,13 +3,13 @@ import env from './_env'
 describe('MainMenuDropdown', () => {
   beforeAll(async() => {
     await page.goto(env.baseUrl);
+    await page.setViewport({ width: 1280, height: 800 });
   });
 
   it('should expand when clicked', async() => {
-    const toggle = await page.$('.cd-main-menu__btn');
-    await page.setViewport({ width: 1280, height: 800 });
+    const toggle = await page.$('button.cd-nav-level-1__btn');
     await toggle.click();
-    const hidden = await page.$eval('.cd-main-menu__dropdown', el => el.dataset.cdHidden);
+    const hidden = await page.$eval('.cd-nav-level-2__dropdown', el => el.dataset.cdHidden);
     await expect(hidden).toMatch('false');
   });
 });
