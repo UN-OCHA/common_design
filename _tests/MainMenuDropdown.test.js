3ff0c6451ac2c6e48fc1337ec2ffb0bd24e87fbf
left23 <lcdemc@gmail.com>
2023-10-04 15:15:11 +0200

    chore: update selectors with new classes so test passes
    Refs: CD-505

--- _tests/MainMenuDropdown.test.js
+++ _tests/MainMenuDropdown.test.js
@@ -3,11 +3,11 @@ import env from './_env'
 describe('MainMenuDropdown', () => {
   beforeAll(async() => {
     await page.goto(env.baseUrl);
-    await page.setViewport({ width: 1280, height: 800 });
   });
 
   it('should expand when clicked', async() => {
-    const toggle = await page.$('button.cd-nav-level-1__btn');
+    const toggle = await page.$('.menu-item--expanded');
+    await page.setViewport({ width: 1280, height: 800 });
     await toggle.click();
     const hidden = await page.$eval('.cd-nav-level-2__dropdown', el => el.dataset.cdHidden);
     await expect(hidden).toMatch('false');
