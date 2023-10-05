3e64b1ef2c6029f5ab9777ebf977bd2e46b6783d
left23 <lcdemc@gmail.com>
2023-10-05 12:32:02 +0200

    style: remove space
    Refs: CD-505

--- common_design_subtheme/_tests/HeadingOne.test.js
+++ common_design_subtheme/_tests/HeadingOne.test.js
@@ -5,7 +5,7 @@ describe('HeadingOne', () => {
     await page.goto(env.baseUrl);
   });
 
-  it('should have a single heading one', async () => {
+  it('should have a single heading one', async() => {
     const headingOne = await page.$$('h1')
     expect(headingOne.length).toBe(1)
   })
